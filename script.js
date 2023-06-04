877cc4d1d7f03f14b34bb597ee6ce6684d5b677f
lcyning <lcyning@icloud.com>
2023-06-04 23:06:11 +1000

    test commit

--- script.js
+++ script.js
@@ -4,7 +4,9 @@ toggle between hiding and showing the dropdown content */
 function myFunction() {
   document.getElementById("myDropdown").classList.toggle("show");
 }
-//
+
+
+///
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
